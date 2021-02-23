cd73f88bf01761030a50f3608282a1fe0892a1b8
krishna saparia <krishnasaparia43045@gmail.com>
2021-02-22 22:46:23 -0500

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+import { ShowInfoComponent } from './show-info/show-info.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchResultsComponent,
+    ShowDetailsComponent,
+    ShowInfoComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
